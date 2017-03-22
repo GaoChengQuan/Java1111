5b7825a107439b061451da86ab64664c09eb175d
GaoChengQuan <529390053@qq.com>
2017-03-22 15:20:39 +0800

    ddd

--- src/com/situ/basic/If.java
+++ src/com/situ/basic/If.java
@@ -17,6 +17,7 @@ public class If {
 			System.out.println("a>b");
 			System.out.println("a>b");
 			System.out.println("a>b");
+			System.out.println("a>b");
 		} else {
 			System.out.println("a<b");
 		}
